da359fd833e59bd53845ad9a98c9757fbbbc4f27
andrew webber <andrew_webber_@hotmail.com>
2014-05-10 00:14:20 +0200

    auth

--- routes/login.js
+++ routes/login.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.send('login');
+	res.send('login');
 });
 
 router.post('/', function(req, res) {
 	console.log('body ' + req.body);
-	var user = JSON.parse(req.body);
-	console.log(user.userName);
-	res.send(user);
+	console.log(req.body.userName);
+	res.send(req.body.userName);
 });
 
 module.exports = router;
