aaa7c2029b278f99edbaf31eb0f2e10a23a79f4e
andrew webber <awebber@originsinfo.eu>
2014-05-11 21:44:44 +0200

    progress 8

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -11,19 +11,51 @@ function DataModel() {
     };
 }
 
-function App(dataModel) {
-    var self = this;
+function ProtectedAreaViewModel(app){
+	var self = this;
+	self.message = ko.observable('protectedArea');
+	Sammy(function(){
+		this.get('#/protected',function(){
+			app.activeViewName('protected');
 
-    self.userName = ko.observable();
-    self.password = ko.observable();
-
-    self.signin = function () {
-        dataModel.login({ userName: self.userName(), password: self.password() })
-		.done(
-		function (data) {
-			alert(data);
+			var accessToken = sessionStorage.getItem('accessToken');
+			if(!accessToken){
+				this.redirect('#/login');
+			}
 		});
-    };
+	});
+}
+
+function LoginViewModel(app, dataModel){
+	var self = this;
+	self.userName = ko.observable();
+	self.password = ko.observable();
+
+	self.signin = function () {
+		dataModel.login({ userName: self.userName(), password: self.password() }).done(
+			function (data) {
+				var accessToken = data.token;
+				sessionStorage.setItem('accessToken', accessToken);
+				var accessToken2 =sessionStorage.getItem('accessToken');
+				console.log(accessToken2);
+				window.location.hash = '#/protected';
+			});
+	};
+
+	Sammy(function(){
+		this.get('#/login',function(){
+			app.activeViewName('login');
+		});
+	});
+}
+
+function App(dataModel) {
+	var self = this;
+	self.loginViewModel = new LoginViewModel(this,dataModel);	
+	self.protectedAreaViewModel = new ProtectedAreaViewModel(this);
+	self.activeViewName = ko.observable('');
+
+	Sammy().run('#/protected');
 }
 
 var app = new App(new DataModel());
